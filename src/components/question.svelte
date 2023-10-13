b5881f20f662731881164cd5ac8090853dae8644
Sourabh Verma <sourabh.coder@gmail.com>
2023-10-14 00:47:35 +0530

    Make button less annoying

--- src/components/question.svelte
+++ src/components/question.svelte
@@ -20,7 +20,7 @@
 			type="checkbox"
 			bind:checked={$checked}
 			name={question.name}
-			class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+			class="h-4 w-4 rounded border-gray-300 accent-indigo-500 focus:accent-indigo-600"
 		/>
 		<label for={question.name} class="text-sm my-0 ml-4 font-semibold leading-none text-gray-900">
 			{question.name}
@@ -29,7 +29,7 @@
 	<button
 		type="submit"
 		on:click={() => window.open(question.link, '_blank')}
-		class="flex flex-row items-center gap-x-2 rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm transition-all hover:shadow-xl hover:bg-indigo-700"
+		class="flex flex-row items-center gap-x-2 rounded-md px-2 py-0.5 text-sm font-semibold leading-6 text-indigo-600 transition-all hover:bg-indigo-100"
 	>
 		<svg
 			width="18px"
@@ -40,7 +40,7 @@
 		>
 			<path
 				d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11"
-				stroke="#fff"
+				class="stroke-indigo-600"
 				stroke-width="2"
 				stroke-linecap="round"
 				stroke-linejoin="round"
