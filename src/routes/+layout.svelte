f88d85af23e7e2a29881f076b0ff616f98c5f4f5
Sourabh Verma <sourabh.coder@gmail.com>
2023-10-15 17:56:13 +0530

    Add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import logo from '../assets/images/logo.svg'
 </script>
 
 <div class="h-full flex flex-col overflow-hidden">
@@ -9,19 +10,19 @@
 				<div class="flex flex-1 items-center">
 					<div class="flex flex-row items-center flex-1">
 						<img
-							class="h-8 w-8 animate-hue"
-							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-							alt="NeetCode"
+							class="h-6 w-6 animate-hue rotate-45 hover:scale-110 transition-all duration-500 ease-in-out"
+							src={logo}
+							alt="NeatCode"
 						/>
-						<h1 class="ml-5 sm:ml-10 text-white text-2xl font-bold tracking-tight">NeetCode</h1>
+						<h1 class="ml-5 sm:ml-10 text-white text-2xl font-bold tracking-tight">NeatCode</h1>
 					</div>
 					<div class="flex flex-row justify-between items-center leading-none">
 						<div class="flex flex-col items-end">
 							<p class="text-gray-300 text-sm font-bold tracking-tight hidden sm:block">
 								A place to practice your coding skills
 							</p>
-							<p class="text-gray-300 text-[12px] mt-1 font-bold tracking-tight">
-								State stored locally
+							<p class="text-gray-300 text-[12px] mt-1 color-indigo-400 font-bold tracking-tight">
+								Login
 							</p>
 						</div>
 					</div>
